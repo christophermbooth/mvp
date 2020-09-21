05dc7e418fd07145f94facc2d218fad5dc0b2587
christophermbooth <booth.christopherm@gmail.com>
2020-09-21 17:13:14 -0500

    Stuff

---
+++ server/db.js
@@ -0,0 +1,49 @@
+// const mysql      = require('mysql');
+// const connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'boredboards'
+// });
+ 
+// connection.connect((err, res) => {
+//   if (err) {
+//     console.log('Something went wrong connection to the database: ', err)
+//     return;
+//   }
+//   console.log('Connected to database')
+
+// });
+
+
+// module.exports = {
+//   connection
+// }
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/boards', {useCreateIndex: true, useNewUrlParser: true});
+
+const gameSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true,
+    index: true
+  },
+  name: String,
+  minPlayers: Number,
+  maxPlayers: Number,
+  minPlayTime: Number,
+  maxPlayTime: Number,
+  averageUserRating: Number,
+  userVotes: {
+    type: Number,
+    default: 0
+  }
+})
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = {
+  Game
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,54 @@
 const dotenv = require('dotenv').config();
+const e = require('express');
 const express = require('express');
 const app = express();
 const {PORT} = process.env;
+const mongoose = require('mongoose')
+const {Game} = require('./db')
+// const connection = require('./db')
 
+//API QUERIES RETURN AN OBJECT WITH GAMES ARRAY CONTAINING OBJECTS
+//EACH OBJECT IS A GAME
+//
 app.listen(PORT, () => console.log(`Server now listening on port: ${PORT}`));
 
 app.get('/', (req, res) => {
   res.send('WELCOME SHITHEAD THIS IS GOING TO BE PAINFUL');
 })
+
+
+app.post('/games', (req, res) => {
+  // console.log(req,'req');
+  let testGame = {
+    id: 45154,
+    name: 'TEST GAME 8000'
+  }
+  Game.find(testGame)
+    .then(result => {
+      if(!result.length) {
+        console.log('Now saving new entry')
+        new Game(testGame).save()
+          .then((savedGame) => {
+            res.status(200)
+            res.send(savedGame);
+            return;
+          })
+      }else{
+        console.log('Entry already exists for game, skipping')
+        res.status(302)
+        res.send();
+      }
+    })
+    .catch((err)=> {
+      console.log('Something went wrong querying the server: ', err)
+    })
+})
+
+
+app.get('/games', (req, res) => {
+  Game.find({})
+    .then(games => {
+      res.status(200).send(games);
+    })
+});
+
