e19a4ca3291932375e5a096a773a5bd1ca90a30a
christophermbooth <booth.christopherm@gmail.com>
2020-09-22 09:42:16 -0500

    Webpack configured correctly, basic react page loading

--- server/index.js
+++ server/index.js
@@ -1,27 +1,30 @@
 const dotenv = require('dotenv').config();
+const path = require('path');
 const e = require('express');
 const express = require('express');
 const app = express();
-const {PORT} = process.env;
+const {PORT} = process.env || 3000;
 const mongoose = require('mongoose')
 const {Game} = require('./db')
-// const connection = require('./db')
 
-//API QUERIES RETURN AN OBJECT WITH GAMES ARRAY CONTAINING OBJECTS
-//EACH OBJECT IS A GAME
-//
+const DIST_DIR = path.join(__dirname, '../dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+app.use(express.static(DIST_DIR))
+
+
 app.listen(PORT, () => console.log(`Server now listening on port: ${PORT}`));
 
 app.get('/', (req, res) => {
-  res.send('WELCOME SHITHEAD THIS IS GOING TO BE PAINFUL');
+  res.send(HTML_FILE);
 })
 
 
 app.post('/games', (req, res) => {
   // console.log(req,'req');
   let testGame = {
-    id: 45154,
-    name: 'TEST GAME 8000'
+    id: 451512,
+    name: 'TEST GAME 9000'
   }
   Game.find(testGame)
     .then(result => {
