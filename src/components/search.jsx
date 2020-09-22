2216f55c3287357578e838e9334ed2a19528b85a
christophermbooth <booth.christopherm@gmail.com>
2020-09-22 10:31:34 -0500

    Converted index to class, created search component that is rendered to the page

---
+++ src/components/search.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div class='search'>
+        <input type="text" />
+        <button>
+          <span>Roll the Dice!</span>
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Search;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const Index = () => {
-  return <div>Welcome to React!</div>;
-};
+import './styles.scss';
+import Search from './components/search.jsx';
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameData : ['Example Game Data'],
+      searchTest : '',
+      gameEntries: []
+    };
+  }
+  render() {
+    return (
+       <div>Welcome to hell MUAHAHAHHAHAH
+         <Search />
+       </div>
+    ) 
+
+  }
+}
+
 ReactDOM.render(<Index />, document.getElementById('root'));
